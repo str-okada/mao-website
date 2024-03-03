40ce703f055cbcc25310b23478e6f39155760b76
str-okada <shotarookadaa@gmail.com>
2024-03-03 14:43:41 +0900

    design part is done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mao-website
+
+2024/3/3
+1, HTML is done
+2, CSS is done
+3, Still on the way to making the scroll sunction
--- js/script.js
+++ js/script.js
@@ -4,5 +4,36 @@ const header = document.querySelector('header');
 
 // when you click h-btn
 hBtn.addEventListener('click', () => {
+    // hBtn.preventDefault()
     header.classList.toggle('active');
+});
+
+const swiper = new Swiper('.swiper', {
+
+    // Optional parameters
+    slidesPerView: 2,
+    spaceBetween: 20,
+    centerdSliders: true,
+    loop: true,
+    breakpoints: {
+        750: {
+            slidesPerView: 4,
+            spaceBetween: 60,
+        }
+    },
+});
+
+// fade in *when you scroll
+window.addEventListener('scroll', () => {
+    const item = document.querySelectorAll('.item');
+    for (let i = 0; i < item.length; i++) {
+        let targetTop = item[i].offsetTop;
+
+        if (window.screenY + 300 > targetTop) {
+            item[i].classList.add('show');
+        }
+        console.log(targetTop);
+        // console.log(window.scrollY);
+
+    }
 });
